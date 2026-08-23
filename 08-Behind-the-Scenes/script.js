e9ff91cf89d5353baaaa0c11efca9ef13c357c7c
unknown <damadmohammad4@gmail.com>
2026-08-24 02:12:34 +0330

    learn some more notes about this key word

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -101,21 +101,69 @@ const calcAgeArrow = birthYear => {
 };
 calcAgeArrow(1980);
 
-const jonas = {
+const jonass = {
   year: 1991,
   calcAge: function () {
     console.log(this);
     console.log(2037 - this.year);
   },
 };
-jonas.calcAge();
+jonass.calcAge();
 
 const matilda = {
   year: 2017,
 };
 
-matilda.calcAge = jonas.calcAge;
+matilda.calcAge = jonass.calcAge;
 matilda.calcAge();
 
-const f = jonas.calcAge;
+const f = jonass.calcAge;
 f();
+
+///////////////////////////////////////
+// Regular Functions vs. Arrow Functions
+// var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExprr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExprr(2, 5);
+addExprr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
